2473b8997d639399c5494e0d9c1af9a5ee35b893
Iris <irisppm@gmail.com>
2022-12-13 20:20:37 -0600

    add route home in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+	const client = new ApolloClient({
+		cache: new InMemoryCache(),
+		uri: "http://localhost:3000/",
+	});
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	return (
+		<Router>
+			<ApolloProvider client={client}>
+
+				<Navbar />
+				<Routes>
+					<Route path="/home" element={<Home />} />
+				</Routes>
+				
+
+			</ApolloProvider>
+		</Router>
+	);
 }
 
-export default App
+export default App;
