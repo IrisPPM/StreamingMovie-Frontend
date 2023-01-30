c07b4452fc50f657738a7e365822882f3e584edc
Iris <irisppm@gmail.com>
2023-01-29 18:55:53 -0600

    modified login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { LOGIN } from "../graphql/Queries";
 import { useLazyQuery } from "@apollo/client";
 import { useNavigate } from "react-router-dom";
+import "./Style.css"
 
 const Login = () => {
 	const navigate = useNavigate();
@@ -19,6 +20,7 @@ const Login = () => {
 	}
 
 	return (
+		<div className="login">
 		<form
 			onSubmit={async (e) => {
                 e.preventDefault();
@@ -37,10 +39,10 @@ const Login = () => {
                 } )
 			}}
 		>
-			<div className="mb-6">
+			<div className="grid gap-6 mb-6">
 				<label
 					for="email"
-					className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+					className="block mb-1 text-sm font-medium text-gray-900 dark:text-white"
 				>
 					Your email
 				</label>
@@ -50,15 +52,15 @@ const Login = () => {
 						setEmail(e.target.value);
 					}}
 					id="email"
-					className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+					className="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 					placeholder="name@gmail.com"
 					required
 				/>
 			</div>
-			<div className="mb-6">
+			<div className="grid w-full">
 				<label
 					for="password"
-					className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+					className="block mb-1 text-sm font-medium text-gray-900 dark:text-white"
 				>
 					Your password
 				</label>
@@ -68,7 +70,8 @@ const Login = () => {
 						setPassword(e.target.value);
 					}}
 					id="password"
-					className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+					className="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full px-10 py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+					placeholder="Password"
 					required
 				/>
 			</div>
@@ -82,11 +85,12 @@ const Login = () => {
 
 			<button
 				type="submit"
-				className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+				className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm w-full sm:w-auto px-10 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 			>
-				Submit
+				Login
 			</button>
 		</form>
+		</div>
 	);
 };
 
